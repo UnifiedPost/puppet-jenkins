65882b6b3b91edb769f819b2431319919c122b5e
Jan Vansteenkiste <jan@vstone.eu>
2012-01-25 20:48:22 +0000

    added jenkins module / original import from inuits repository
    git-svn-id: http://svn.dev.unifiedpost.com/repos/it/puppet/modules/jenkins/trunk@2 541fe5b8-0cc0-4adb-9f2e-9c570b39a0ab

---
+++ manifests/init.pp
@@ -0,0 +1,101 @@
+
+class jenkins::enabled  {
+ service{"jenkins":
+        ensure => "running" ,
+        enable => "true",
+    }
+}
+
+# EOC jenkins::enabled 
+class jenkins::disabled {
+ service {"jenkins":
+        ensure => "stopped" ,
+        enable => "false",
+    }
+
+}
+# EOC jenkins::disabled 
+
+
+class jenkins::repo {
+
+	yumrepo {
+   	"jenkins":
+            descr       => "jenkins",
+            enabled     => 1,
+            gpgcheck    => 0,
+            baseurl     => "http://pkg.jenkins-ci.org/redhat/"; 
+        
+
+	}
+
+
+}
+
+class jenkins::package {
+	package {
+		"jenkins":
+		     ensure => present;
+	}
+
+
+
+}
+
+class jenkins{
+
+	include jenkins::repo
+	include jenkins::package
+	Class["jenkins::repo"] -> Class["jenkins::package"]
+	include jenkins::enabled 
+	include jenkins::plugins 
+
+}
+
+
+class jenkins::plugins{
+
+	  yumrepo { "inuits-jenkins-plugins":
+            descr       => "inuits-jenkins-plugins",
+            enabled     => 1,
+            gpgcheck    => 0,
+            baseurl     => "http://repo.inuits.be/jenkins/os";
+
+        }
+
+
+	package {
+		"jenkins-analysis-core-plugin":
+		     ensure => present;
+		"jenkins-checkstyle-plugin":
+		     ensure => present;
+		"jenkins-clover-plugin":
+		     ensure => present;
+		"jenkins-doxygen-plugin":
+		     ensure => present;
+		"jenkins-dry-plugin":
+		     ensure => present;
+		"jenkins-git-plugin":
+		     ensure => present;
+		"jenkins-htmlpublisher-plugin":
+		     ensure => present;
+		"jenkins-jdepend-plugin":
+		     ensure => present;
+		"jenkins-plot-plugin":
+		     ensure => present;
+		"jenkins-pmd-plugin":
+		     ensure => present;
+		"jenkins-promoted-builds-plugin":
+		     ensure => present;
+		"jenkins-publish-over-ssh-plugin":
+		     ensure => present;
+		"jenkins-violations-plugin":
+		     ensure => present;
+		"jenkins-xunit-plugin":
+		     ensure => present;
+
+	}
+
+
+}	
+
