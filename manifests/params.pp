1c658c0e1b17a9447a75818d6b18a16290c0fb6d
Jan Vansteenkiste <jan@vstone.eu>
2012-01-26 09:18:19 +0100

    Added operating system name that is not supported for a better user experience

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,7 +21,7 @@ class jenkins::params(
         /(?i:centos|redhat)/: { $java = 'java-1.6.0-openjdk' }
         /(?i:debian|ubuntu)/: { $java = 'openjdk-6-jdk' }
         default: {
-          fail("The jenkins module has no java dependency configured for your operatingsystem.")
+          fail("The jenkins module has no java dependency configured for your operatingsystem: ${::operatingsystem}")
         }
       }
     }
