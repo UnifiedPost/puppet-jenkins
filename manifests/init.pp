8d07657760c57c63c4a3aba49770eec02dae450b
jvansteenkiste <jvansteenkiste@541fe5b8-0cc0-4adb-9f2e-9c570b39a0ab>
2012-01-26 08:31:03 +0000

    Removed the hard coded jenkins plugins and create a wrapper definition for plugins
    git-svn-id: http://svn.dev.unifiedpost.com/repos/it/puppet/modules/jenkins/trunk@8 541fe5b8-0cc0-4adb-9f2e-9c570b39a0ab

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,46 +1,6 @@
 class jenkins{
 
-	include jenkins::package
-	include jenkins::service
-  ##	include jenkins::plugins
+  include jenkins::package
+  include jenkins::service
 
 }
-
-
-class jenkins::plugins{
-
-	package {
-		"jenkins-analysis-core-plugin":
-		     ensure => present;
-		"jenkins-checkstyle-plugin":
-		     ensure => present;
-		"jenkins-clover-plugin":
-		     ensure => present;
-		"jenkins-doxygen-plugin":
-		     ensure => present;
-		"jenkins-dry-plugin":
-		     ensure => present;
-		"jenkins-git-plugin":
-		     ensure => present;
-		"jenkins-htmlpublisher-plugin":
-		     ensure => present;
-		"jenkins-jdepend-plugin":
-		     ensure => present;
-		"jenkins-plot-plugin":
-		     ensure => present;
-		"jenkins-pmd-plugin":
-		     ensure => present;
-		"jenkins-promoted-builds-plugin":
-		     ensure => present;
-		"jenkins-publish-over-ssh-plugin":
-		     ensure => present;
-		"jenkins-violations-plugin":
-		     ensure => present;
-		"jenkins-xunit-plugin":
-		     ensure => present;
-
-	}
-
-
-}	
-
---
+++ manifests/plugin.pp
@@ -0,0 +1,19 @@
+# = Definition: jenkins::plugin
+#
+# Install a jenkins module with ease.
+#
+# == Parameters:
+#
+# $ensure::     Ensure wether a plugin should be present or absent.
+#               Defaults to 'present'.
+#
+define jenkins::plugin(
+  $ensure = 'present'
+) {
+
+  package {"jenkins-plugin-${name}":
+    ensure => $ensure,
+  }
+
+}
+
