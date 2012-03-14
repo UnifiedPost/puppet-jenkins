0b9438c5c4d76cc3b5b992f1539f8af32d13aeb8
Jan Vansteenkiste <jan@vstone.eu>
2012-03-14 10:30:28 +0100

    Remove some spammy comments

--- manifests/package.pp
+++ manifests/package.pp
@@ -5,12 +5,8 @@
 class jenkins::package {
 
   include jenkins::params
-
-  ## Group variables we will use from params
-  $package = $jenkins::params::package
-  $java = $jenkins::params::java
-
-  #
+  $package  = $jenkins::params::package
+  $java     = $jenkins::params::java
 
   if ! defined(Package[$java]) {
     package {$java:
