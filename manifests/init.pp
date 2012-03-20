68d505b21fffe850511453b77fea4e3981055689
Jan Vansteenkiste <jan@vstone.eu>
2012-03-20 15:16:47 +0100

    Move module information out of init into module.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,4 @@ class jenkins {
   include jenkins::package
   include jenkins::service
 
-  $modulename = 'up-puppet-jenkins'
-  $moduleversion = '0.1dev'
-
 }
---
+++ manifests/module.pp
@@ -0,0 +1,11 @@
+# = Class: jenkins::module
+#
+# Module version information.
+#
+class jenkins::module {
+
+  $id = 'vstone-puppet-jenkins'
+  $version = '0.1'
+
+}
+
